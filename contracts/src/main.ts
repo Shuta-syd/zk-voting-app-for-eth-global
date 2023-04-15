9550427258f54f8e2bef84c6e0dbb04945d7d5e7
Shuta-syd <shuta.ogura0921@gmail.com>
2023-04-15 12:31:52 +0900

    [update] main.ts in contracts

--- contracts/src/main.ts
+++ contracts/src/main.ts
@@ -1,9 +1,11 @@
 import { isReady, shutdown } from "snarkyjs";
 
 async function main() {
-  await isReady();
+  await isReady;
 
   console.log('Snarkjs loaded')
 
   await shutdown();
 }
+
+main();
