fd148989dd030f42b28c51aef559979760a70139
yusei399 <yusei2021@keio.jp>
2023-04-16 00:19:20 +0900

    投票機能プロトタイプ

--- ui/src/pages/index.page.tsx
+++ ui/src/pages/index.page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { useEffect, useState } from 'react';
 import type { Add } from '../../../contracts/src/';
+import CreateVote from './createvote.page';
 import {
   Mina,
   isReady,
@@ -16,15 +17,25 @@ import {
   FormControl,
   Input,
   Button,
+  ChakraProvider,
 } from '@chakra-ui/react'
+import { theme } from '@chakra-ui/react';
 import Link from 'next/link';
+import VoteList from "./voteList.page";
+
+
 
 export default function Home() {
   return(
     <>
-      <Link href="/createvote">
-        <Button colorScheme="teal">投票作成</Button>
-      </Link>
+    <ChakraProvider theme={theme}>
+        <Link href="/createvote">
+          <Button colorScheme="teal">投票作成</Button>
+        </Link>
+        <Link href="/voting">
+          <Button colorScheme="teal">投票</Button>
+        </Link>
+      </ChakraProvider>
     </>
   );
 };
